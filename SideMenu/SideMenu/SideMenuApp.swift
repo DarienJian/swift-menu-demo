1a0e40c1bd94b59e9c1b063d7832b6b9c343cbe2
Darien <r880514@gmail.com>
2021-05-15 16:14:48 +0800

    建構

---
+++ SideMenu/SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Akira Akimoto on 2021/5/15.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
